ad085815c66488cdf854461c5dec1fa8e7195161
Mustafa <971530+tosbaha@users.noreply.github.com>
2022-05-27 15:21:24 +0300

    Changed bundle id

--- android/app/src/main/kotlin/com/example/appcircle_sample_flutter/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/appcircle_sample_flutter/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.appcircle_sample_flutter
+package com.example.appcircle_sample_flutter_google_submit_app
 
 import io.flutter.embedding.android.FlutterActivity
 
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:appcircle_sample_flutter/main.dart';
+import 'package:appcircle_sample_flutter_google_submit_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
